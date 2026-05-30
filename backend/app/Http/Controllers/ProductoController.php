5a22d25e579e7f1185626e01dc6e610eec625490
Jorge Garcia <jorgeg23600@gmail.com>
2026-05-29 21:43:58 -0600

    Creacion de endpoint para listar productos

---
+++ backend/app/Http/Controllers/ProductoController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Producto;
+
+class ProductoController extends Controller
+{
+    public function index()
+    {
+        return response()->json(
+            Producto::all()
+        );
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductoController;
+
+Route::get('/productos', [ProductoController::class, 'index']);
 
 Route::get('/', function () {
 

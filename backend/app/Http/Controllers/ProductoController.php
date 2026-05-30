009457d9c6ce1bc0be9699f451640e8e7297c85b
Jorge Garcia <jorgeg23600@gmail.com>
2026-05-29 23:46:21 -0600

    Implementacion de creacion de productos mediante api rest

--- backend/app/Http/Controllers/ProductoController.php
+++ backend/app/Http/Controllers/ProductoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Producto;
+use Illuminate\Http\Request;
 
 class ProductoController extends Controller
 {
@@ -11,5 +12,17 @@ class ProductoController extends Controller
         return response()->json(
             Producto::all()
         );
+    }
+
+    public function store(Request $request)
+    {
+        $producto = Producto::create([
+            'nombre' => $request->nombre,
+            'descripcion' => $request->descripcion,
+            'precio' => $request->precio,
+            'stock' => $request->stock,
+        ]);
+
+        return response()->json($producto, 201);
     }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductoController;
 
 Route::get('/productos', [ProductoController::class, 'index']);
+Route::post('/productos', [ProductoController::class, 'store']);
 
 Route::get('/', function () {
 
